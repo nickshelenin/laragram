96b4e11f091309f01efdd23528ab0d8a11b7b2ec
nick <nickshelenin@gmail.com>
2020-04-17 10:20:08 +0600

    bootstrap link provided

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,6 +19,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ secure_asset('css/AdminLTE.min.css') }}">
 </head>
 
 <body>
